55bacbba42f8104c730fe6ab76d738c24bc0972f
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2016-02-10 17:54:54 +0300

    markup changed

--- markup/js/angular_script.js
+++ markup/js/angular_script.js
@@ -1,39 +1,74 @@
+var meet = {
+    name: "Meet",
+    time: new Date(1970, 0, 1),
+    description: "You are always welcome.",
+    type: "remote",
+    targets: ["Whe everything sucks?"],
+    meet_time: new Date(1970, 0, 1)
+};
 
-	var meetmodel = {
-	group_id: 0, 
-	meet_id: 0,
-	meet_name: "default_name",
-	meet_descr: "default_descr",
-	meet_type: "remote",
-	meet_date: "06/02/2016",
-	meet_time: "18:00",
-	meet_address: "default_address",	
-	meet_goals: 
-	[],
-	meet_comments:"default_comments"
-	};
-	var creatingNewMeet = angular.module("creatingNewMeet",[])
-	.controller("creatingNewMeetCtrl", function($scope){
-		$scope.newMeet = meetmodel;
-		$scope.addNewMeet = function(){			
-			$scope.newMeet.meet_name = $scope.txtName;
-			$scope.newMeet.meet_descr = $scope.txtDescr;
-			$scope.newMeet.meet_type = $scope.selType;
-			$scope.newMeet.meet_date = $scope.txtDate;
-			$scope.newMeet.meet_time = $scope.txtTime;
-			$scope.newMeet.meet_address = $scope.txtAddress;
-			$scope.newMeet.meet_сomments = $scope.txtComm;
-			var elem = angular.element(document.querySelector("#meet_goal"));
-			var goals = elem.children().length;
-			for(var i = 0; i < elem.children().length - 1; i++){
-				var totalText = elem.children().eq(i).text();
-				var startIndex = totalText.indexOf('grade');
-				var endIndex = totalText.indexOf('clear');
-				var goalText = totalText.substr(startIndex + 5,endIndex - startIndex - 5);
-				alert(goalText);
-				$scope.newMeet.meet_goals.push({
-					goal: goalText
-				})
-			}
-		}
-  });	
+var app = angular.module('app', ['ngMaterial'])
+    .config(function ($mdThemingProvider) {
+        $mdThemingProvider.definePalette('amazingPaletteName', {
+            '50': '2e7d32',
+            '100': '2e7d32',
+            '200': '2e7d32',
+            '300': '2e7d32',
+            '400': '2e7d32',
+            '500': 'ef6c00',
+            '600': '2e7d32',
+            '700': '2e7d32',
+            '800': '2e7d32',
+            '900': '2e7d32',
+            'A100': '2e7d32',
+            'A200': '2e7d32',
+            'A400': '2e7d32',
+            'A700': '2e7d32',
+            'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+            // on this palette should be dark or light
+            'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
+                '200', '300', '400', 'A100'],
+            'contrastLightColors': undefined    // could also specify this if default was 'dark'
+        });
+        $mdThemingProvider.theme('default')
+            .primaryPalette('amazingPaletteName')
+    });
+
+app.controller("editMeetCtrl", function ($scope) {
+    $scope.meet = meet;
+    $scope.createTarget = function () {
+        $scope.meet.targets.push($scope.target);
+        $scope.target="";
+    }
+    $scope.removeTarget = function (target) {
+        $scope.meet.targets.splice( $scope.meet.targets.indexOf(target), 1);
+    }
+});
+
+
+app.controller('meetListCtrl', function ($scope) {
+    var meet1 = {
+        name: "Meet",
+        time: new Date(1970, 11, 12, 14, 50),
+        description: "You are always welcome.",
+        type: "remote"
+    };
+    var meet2 = {
+        name: "Meet2",
+        time: new Date(1970, 11, 12, 14, 50),
+        description: "You are always welcome.",
+        type: "remote"
+    };
+    var meet3 = {
+        name: "Meet3",
+        time: new Date(1970, 11, 12, 14, 50),
+        description: "You are always welcome.",
+        type: "remote"
+    };
+
+    $scope.meets = [meet1, meet2, meet3];
+
+    $scope.editMeet = function () {
+        window.location = "edit_meet.html";
+    };
+});
